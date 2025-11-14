ff22dbab970bebbfdefc152df94ae2b7531e1ced
Mohammad Salman Ansari <138972622+mSalman101@users.noreply.github.com>
2025-11-14 20:57:29 +0530

    Add files via upload

---
+++ 2G/Lec10(lab)/HierarchicalInheritanceDemo.java
@@ -0,0 +1,25 @@
+class Animal { 
+void eat() { 
+System.out.println("Eating..."); 
+} 
+} 
+class Dog extends Animal { 
+void bark() { 
+System.out.println("Barking..."); 
+} 
+} 
+class Cat extends Animal { 
+void meow() { 
+System.out.println("Meowing..."); 
+} 
+} 
+public class HierarchicalInheritanceDemo { 
+public static void main(String[] args) { 
+Dog d = new Dog(); 
+d.eat(); 
+d.bark(); 
+Cat c = new Cat(); 
+c.eat(); 
+c.meow(); 
+} 
+} 
